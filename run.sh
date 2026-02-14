004620833e91d104bc7509d0f848b28dffa5e866
p4ken <commit@p4ken.skr.jp>
2026-02-14 18:13:34 +0900

    Refactor runner script

--- run.sh
+++ run.sh
@@ -3,20 +3,12 @@
 set -xeu
 
 APP_NAME="NEXGallery"
-APP_DIR="$APP_NAME.app"
+BUNDLE_DIR="$APP_NAME.app"
 
-# 1. コンパイル (swiftcコマンドを使用)
-echo "Compiling..."
-swiftc *.swift -o $APP_NAME
+# Build
+mkdir -p "$BUNDLE_DIR/Contents/MacOS"
+swiftc *.swift -o "$BUNDLE_DIR/Contents/MacOS/$APP_NAME"
+cp Info.plist "$BUNDLE_DIR/Contents/Info.plist"
 
-# 2. .appディレクトリ構造の作成
-mkdir -p "$APP_DIR/Contents/MacOS"
-mkdir -p "$APP_DIR/Contents/Resources"
-
-# 3. 実行ファイルとInfo.plistの配置
-mv $APP_NAME "$APP_DIR/Contents/MacOS/"
-cp Info.plist "$APP_DIR/Contents/Info.plist"
-
-# 4. アプリケーションの起動
-echo "Launching..."
-open "$APP_DIR"
+# Lanuch
+open "$BUNDLE_DIR"
